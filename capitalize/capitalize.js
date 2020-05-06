f5274b4aa3391b4dcbf8d4fa43d29565b40c3f0c
Juan Pablo <jpablomgil@gmail.com>
2020-05-06 15:43:41 -0500

    Fixing offences Eslint on caesar and capitalyze functions

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,7 +1,7 @@
 function capitalize(word) {
-    if (typeof word != 'string') {return 'Invalid string'}
-    let newWord = word.charAt(0).toUpperCase() + word.slice(1)
-    return newWord
+  if (typeof word !== 'string') { return 'Invalid string'; }
+  const newWord = word.charAt(0).toUpperCase() + word.slice(1);
+  return newWord;
 }
 
-module.exports = capitalize
+module.exports = capitalize;
--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -1,9 +1,9 @@
 const capitalize = require('./capitalize');
 
 test('Capitalize parameter its not a string ', () => {
-    expect(capitalize(1)).toBe('Invalid string')
+  expect(capitalize(1)).toBe('Invalid string');
 });
 
 test('Capitalize parameter its a valid string ', () => {
-    expect(capitalize('juan')).toBe('Juan')
+  expect(capitalize('juan')).toBe('Juan');
 });
